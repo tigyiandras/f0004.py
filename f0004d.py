a3a6478fc58295c4d55f6bb8787655cc31b6354c
tigyiandras <71698671+tigyiandras@users.noreply.github.com>
2020-11-03 12:46:15 +0100

    Create f0004d.py

---
+++ f0004d.py
@@ -0,0 +1,5 @@
+magyar_mérföld = input('Hány magyar mérföldre van a sárkány barlangja? ')
+magyar_mérfüld = int (magyar_mérföld)
+km = magyar_mérföld * 8.3536
+tengeri_mérföld = km / 1.852
+print('Ez a táv', km, 'kilométer és', tengeri_mérföld, 'tengeri mérföldet jelent.')
