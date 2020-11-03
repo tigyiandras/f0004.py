abf22b1df8281a7a2ef8714684e19c9c3b1f1402
tigyiandras <71698671+tigyiandras@users.noreply.github.com>
2020-11-03 12:38:01 +0100

    Create f0004b.py

---
+++ f0004b.py
@@ -0,0 +1,9 @@
+név = input('Hogy híjják kendet? ')
+kor = input('És hány éves kend? ') # itt még stringet kapunk
+kor = int(kor) # átalakítjuk egész számmá, azaz int-té
+év = input('Melyik évben járunk? ')
+év = int(év)
+születési_év = év - kor
+print(név, ', kend ', születési_év, '-ban született.', sep='')
+érettségi_év = születési_év + 18
+print(név, ', kend ', érettségi_év, '-ben érettségizik.', sep='')
